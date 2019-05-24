2d326df1ffb8d1b5ef397a991b459dae90783afd
renc <583816993@qq.com>
2019-05-24 16:24:08 +0800

    用来根据数据库表生成对应的实体类、接口、以及mapper映射文件

---
+++ src/main/java/com/glodon/mybatis_entity/MybatisEntityApplication.java
@@ -0,0 +1,13 @@
+package com.glodon.mybatis_entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisEntityApplication.class, args);
+    }
+
+}
